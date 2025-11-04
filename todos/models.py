5d9869f52eac24ce1c3f5e12215945c240dc10bb
ashish-a46 <ashish.bcastudent23.9600@cimage.in>
2025-11-04 11:15:44 +0530

    Added

--- todos/models.py
+++ todos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class To(models.Model):
+class Todoo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
