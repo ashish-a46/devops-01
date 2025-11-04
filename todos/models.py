7ba74c110e1f04ec68b7f1da8f57a7c94753bc9b
ashish-a46 <ashish.bcastudent23.9600@cimage.in>
2025-11-04 11:05:58 +0530

    Added

--- todos/models.py
+++ todos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Todo(models.Model):
+class To(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
